1b7e7a9f1f85bfd80b4f248e5946c0982699178a
jzhu0601 <accazk1@gmail.com>
2016-01-18 16:27:36 -0500

    Spring MVC application that detects the type of device that is accessing your web site and that switches views dynamically based on that device type.

---
+++ src/main/java/hello/Application.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+
+/**
+ * Created by Jason on 1/18/16.
+ */
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
